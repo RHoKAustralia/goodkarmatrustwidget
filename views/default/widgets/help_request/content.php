03f366a274dfb688f068379dc5219258f8dd9bb9
Daniel Coady <daniel.coady@outlook.com.au>
2019-05-19 15:05:21 +1000

    made the help request widget do that thing where it works

--- views/default/widgets/help_request/content.php
+++ views/default/widgets/help_request/content.php
@@ -1,11 +1,8 @@
 <?php
 
-$output = "<table>" .
-          "    <tr><th>I Need Help With...</th></tr>"
-          "    <tr><td>I'm new to the area, what's the best childcare centre?</td></tr>" .
-          "    <tr><td>Just ran out of milk, could anyone spare some?</td></tr>" .
-          "    <tr><td>Looking for cheap second hand furniture</td></tr>" .
-          "</table>"
-
-echo $output;
-
+echo "<table>";
+echo "    <tr style='border-bottom: 1px solid black'><th style='padding: 5px; font-weight: bold; font-size: 20px'>I Need Help With...</th></tr>";
+echo "    <tr style='border-bottom: 1px solid black'><td style='padding: 5px'>I'm new to the area, what's the best childcare centre?</td></tr>";
+echo "    <tr style='border-bottom: 1px solid black'><td style='padding: 5px'>Just ran out of milk, could anyone spare some?</td></tr>";
+echo "    <tr style='border-bottom: 1px solid black'><td style='padding: 5px'>Looking for cheap second hand furniture</td></tr>";
+echo "</table>";
