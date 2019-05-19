24d1bd01a8ac1049a8bdb10a9a0b2de3641af27c
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2019-05-19 14:13:34 +1000

    Mocked up trust metric widget

--- views/default/widgets/trust_metric/content.php
+++ views/default/widgets/trust_metric/content.php
@@ -1,4 +1,15 @@
 <?php
 
-echo "Trust me, I'm awesome!";
+echo "<table style='border:0; cellpadding:5px;width:100%;'>";
+echo "<tr>";
+echo "<td style='text-align: center;margin: auto;'><img src='https://evilangelpixie.nyc3.digitaloceanspaces.com/good-karma/good-karma/Scuba-Diving-18.png'></td>";
+echo "<td style='text-align: center;margin: auto;'><img src='https://evilangelpixie.nyc3.digitaloceanspaces.com/good-karma/good-karma/SEO-01.png'></td>";
+echo "<td style='text-align: center;margin: auto;'><img src='https://evilangelpixie.nyc3.digitaloceanspaces.com/good-karma/good-karma/Chat_Messages-04.png'></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td style='text-align: center;margin: auto;'><b style='font-size:2em'>13</b><br />times helped</td>";
+echo "<td style='text-align: center;margin: auto;'><b style='font-size:2em'>10</b><br />requests made</td>";
+echo "<td style='text-align: center;margin: auto;'><b style='font-size:2em'>12</b><br />thanks received</td>";
+echo "</tr>";
+echo "</table>";
 
