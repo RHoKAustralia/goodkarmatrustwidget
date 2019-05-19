fb7aef95400cec85b925d41cc25036220fbcd740
Daniel Coady <daniel.coady@outlook.com.au>
2019-05-19 14:12:47 +1000

    added help requests widget

---
+++ views/default/widgets/help_request/content.php
@@ -0,0 +1,11 @@
+<?php
+
+$output = "<table>" .
+          "    <tr><th>I Need Help With...</th></tr>"
+          "    <tr><td>I'm new to the area, what's the best childcare centre?</td></tr>" .
+          "    <tr><td>Just ran out of milk, could anyone spare some?</td></tr>" .
+          "    <tr><td>Looking for cheap second hand furniture</td></tr>" .
+          "</table>"
+
+echo $output;
+
